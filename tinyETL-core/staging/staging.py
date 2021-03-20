008ed11c909b630717948ef94c7e84d95393c257
SikRick <drax0x01.dev@gmail.com>
2021-03-20 16:33:16 +0530

    Added staging code

--- tinyETL-core/staging/staging.py
+++ tinyETL-core/staging/staging.py
@@ -1,16 +1,21 @@
 """
 This module has classes and methods to supportstaging ie; loading a specified flatfile to a specified database
+
+R 0.1 : only reads local files, only allows csv files
 """
 __author__ = "SikRick"
 
 import findspark
+findspark.init()
+from pyspark.sql import SparkSession
+
 
 class Stage:
     """
     Stage is a class built to handle staging the flatfile to a specified database.
     """
 
-    def __init__(self, file_path:str, table_name:str, database:str, replace:bool=False, delimiter:chr=",", headers:bool=True):
+    def __init__(self, file_path:str, table_name:str, database:str, replace:bool=False, delimiter:chr=",", header:bool=True, session:str="training"):
         """
         params 
         ------
@@ -20,12 +25,20 @@ class Stage:
         replace : flag to specify to delete exisitng stages tables
         delimiter : specifying the delimiter of the file, defaulted to ','
         headers : flag to specify if headers are present in the file
+        session : SparkSession name ; defaulted to training <might change in future releases>
         """
         self.file_path = file_path
         self.table_name = table_name
         self.database = database
         self.replace = replace
         self.delimiter = delimiter
-        self.headers = headers
+        self.header = header
+        self.session = session
 
-    
+    def load_to_staging_tables(self):
+        spark = SparkSession.builder.appName(self.session).getOrCreate()
+        try:
+            df = spark.read.options(header=self.header, delimiter=self.delimiter).csv(self.file_path)
+            print(df.show())
+        except FileNotFoundError:
+            print("Error finding the specified file!!!")
